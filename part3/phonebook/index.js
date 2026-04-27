e2cf62f4eaaa24c2e7d1105294b5a8a540dc3591
Sabina <sabinachauhan4008@gmail.com>
2026-04-26 21:00:59 -0700

    First Steps of Deployment

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -8,8 +8,8 @@ morgan.token('data', function getData(req, res){
   return JSON.stringify(req.body)
 })
 
- app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static('dist'))
 // Data 
 let persons = [
     { 
